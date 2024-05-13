4afb24adebf81f8b25851180c42ac5b1d91e44c6
CREATI <75898440+CREATI38@users.noreply.github.com>
2024-05-13 14:22:28 +0800

    Revert Test

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*If revert is successful, this sentence shall disappear*/ 
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
